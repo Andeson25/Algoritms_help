2b9790e005da8b6173bb3b7dbfbefa44b896da2c
kek <oleh.petryk@gmail.com>
2018-06-14 05:03:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+for (var i = 0; i < document.getElementsByClassName("item").length; i++) {
+  document.getElementsByClassName("item")[i].lastElementChild.style.display = 'none';
+}
+let keys = document.querySelectorAll(".item");
+keys.forEach(el => {
+  el.addEventListener("click", activated);
+});
+var index=-1;
+function activated(e) {
+  if(index===parseInt(this.firstElementChild.innerHTML.split(')')[0]) -1&& keys[index].lastElementChild.style.display ==='block'){
+     keys[index].lastElementChild.style.display = 'none';
+     return;
+  }else
+   if(index!=-1){
+     keys[index].lastElementChild.style.display = 'none';
+  }
+  e.preventDefault();
+  this.lastElementChild.style.display = 'block';
+  index=parseInt(this.firstElementChild.innerHTML.split(')')[0])-1;
+  console.log(index);
+
+}
